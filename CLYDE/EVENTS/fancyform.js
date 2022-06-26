0a10786027a8622e4ba0c60bc533dd8fb8d60000
madhuresh <minoshideveloper8@gmail.com>
2022-06-26 17:49:31 +0530

    from submit event

---
+++ CLYDE/EVENTS/fancyform.js
@@ -0,0 +1,28 @@
+let output = document.getElementById('output');
+
+let nameText = document.querySelector('#name');
+
+document.querySelector('form').addEventListener('submit',
+(event)=>{
+    event.preventDefault()
+    output.textContent = `A submit event has been fired at ${event.timeStamp}`
+}
+)
+
+nameText.addEventListener('input', ()=>{
+    output.textContent = `The value entered is ${nameText.value}`
+})
+
+nameText.addEventListener('focus',(e)=>{
+    e.target.style.background = 'lightblue';
+    e.target.style.color = 'black';
+
+
+})
+
+nameText.addEventListener('paste',(e)=>{
+    alert('YOU HAVE COPIED!!!!!!!')
+
+
+})
+
