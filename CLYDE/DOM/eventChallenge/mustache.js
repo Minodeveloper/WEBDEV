1061ac9eb30167ab2cb010194f5392996bc718d1
madhuresh <minoshideveloper8@gmail.com>
2022-06-28 21:07:04 +0530

    progressing

--- CLYDE/DOM/eventChallenge/mustache.js
+++ CLYDE/DOM/eventChallenge/mustache.js
@@ -1,9 +1,8 @@
-let face = document.querySelector('#face');
-let mooch =  document.querySelector('#mooch');
+// let face = document.querySelector('#face');
+// let mustache =  document.querySelector('#mustache');
 
-face.addEventListener('click', (e)=>{
-    // console.log( mooch.style);
-    // console.log(e.clienX, e.clientY);
-    mooch.style.top = `${e.clientY-174/2}px`;
-    mooch.style.left = `${e.clientX - 321/2}px` ;
-})
+// face.addEventListener('click', (e)=>{
+
+//     mustache.style.top = `${e.clientY-174/2}px`;
+//     mustache.style.left = `${e.clientX - 321/2}px` ;
+// })
