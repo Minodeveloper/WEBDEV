47fd9c61118273549304b92293420f180be969a8
madhuresh <minoshideveloper8@gmail.com>
2022-07-14 13:45:33 +0530

    clock css js

--- CANVAS/drawCanvas.js
+++ CANVAS/drawCanvas.js
@@ -1,9 +1,48 @@
-myCanvas = document.querySelector('#canv');
+const canvas = document.querySelector('#canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-let context = myCanvas.getContext('2d');
-console.log(context );
+window.addEventListener('resize', ()=>{
+    canvas.width = window.innerWidth;
 
-context.beginPath();
-context.arc(200, 200, 93, Math.PI, 2*Math.PI, false);
-context.fillStyle = '#FF6A6A';
-context.fill();
+    canvas.height = window.innerHeight;
+
+})
+
+const mouse = {
+    x:null,
+    y:null
+}
+
+canvas.addEventListener('click', function(event){
+    mouse.x = event.x;
+    mouse.y = event.y;
+
+})
+canvas.addEventListener('mousemove', animate)
+
+function drawCircle(e){
+    let r = Math.random()*255;
+    let g = Math.random()*255;
+    let b = Math.random()*255;
+    
+    
+    ctx.fillStyle = `rgb(${r},${g},${b})`;
+    // ctx.fillStyle = 'green';
+    
+    ctx.beginPath();
+    ctx.arc(e.x,e.y,20,0, Math.PI*2);
+    ctx.fill()
+    
+}
+
+function animate(e) {
+    ctx.clearReact(0,0,canvas.width, canvas.height);
+    drawCircle(e);
+    requestAnimationFrame(animate);
+    
+}
+
+animate()
+
